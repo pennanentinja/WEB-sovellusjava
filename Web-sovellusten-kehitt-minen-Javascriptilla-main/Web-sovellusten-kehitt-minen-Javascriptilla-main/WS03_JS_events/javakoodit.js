02da1fbe0bd23b0978ee5cb6a5ea988f780ce6f9
pennanentinja <tinja.pennanen@student.laurea.fi>
2025-10-29 13:13:42 +0200

    ws3

--- Web-sovellusten-kehitt-minen-Javascriptilla-main/Web-sovellusten-kehitt-minen-Javascriptilla-main/WS03_JS_events/javakoodit.js
+++ Web-sovellusten-kehitt-minen-Javascriptilla-main/Web-sovellusten-kehitt-minen-Javascriptilla-main/WS03_JS_events/javakoodit.js
@@ -40,3 +40,44 @@ function showTable() {
 
     document.querySelector('#taulukonPaikka').innerHTML = tableHtml;
 }
+
+const otsikko1 = document.querySelector("#harj1");
+
+otsikko1.addEventListener("click", function() {
+    otsikko1.style.color = "red";
+    otsikko1.innerHTML = "Bye bye mouse!";
+});
+
+const tekstialue = document.getElementById("textdata");
+const charcount = document.getElementById("charcount");
+
+tekstialue.addEventListener("focus", function () {
+    charcount.innerHTML = "Please fill in the form with proper data.";
+    tekstialue.style.backgroundColor = "lightgreen";
+})
+
+tekstialue.addEventListener("keydown", () => {
+    const pituus = tekstialue.value.length + 1;
+    charcount.innerHTML = "Please fill in the form with proper data. <br> Merkkejä: " + pituus;
+});
+
+const lomake = document.querySelector("form");
+
+lomake.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const arvo = document.getElementById("textdata").value;
+
+    if (arvo === "") {
+        alert("Kenttä ei voi olla tyhjä!");
+    } else {
+        alert("Lomake lähetetty onnistuneesti!");
+    }
+});
+
+const koordinaatit = document.getElementById("coordinates");
+
+koordinaatit.addEventListener("mousemove", (e) => {
+    console.log("X:", e.offsetX, "Y", e.offsetY);
+    document.getElementById("coords").innerHTML = `X: ${e.offsetX}, Y: ${e.offsetY}`;
+});
